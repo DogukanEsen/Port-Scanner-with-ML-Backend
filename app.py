ea869c8e8c95b0c63a412a72f464e620916db1d5
Satı Doğukan ESEN <73665676+DogukanEsen@users.noreply.github.com>
2023-10-07 19:53:37 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,38 +1,30 @@
-from flask import Flask,request,jsonify,make_response
+from flask import Flask,request,jsonify
 import pickle
 import numpy as np
-from flask_cors import CORS
+from flask_cors import cross_origin
 import re
 
 model = pickle.load(open("model_pickle","rb"))
 
 app = Flask(__name__)
-cors = CORS(app, origins= ["https://ml-port-scanner.netlify.app/","https://ml-port-scanner.netlify.app/"])
 
-@app.route("/predict",methods=["POST","OPTIONS"])
+@app.route("/predict",methods=["POST"])
+@cross_origin(origins=["https://ml-port-scanner.netlify.app/"])
 def predict():
-    if request.method == "OPTIONS":
-        # Preflight isteğine yanıt ver
-        response = make_response()
-        response.headers.add("Access-Control-Allow-Origin", "https://ml-port-scanner.netlify.app")
-        response.headers.add("Access-Control-Allow-Headers", "Content-Type")
-        response.headers.add("Access-Control-Allow-Methods", "POST")
-        return response
+    test=request.get_json()
+    if test is None:
+        return jsonify({"error": "Geçersiz istek, veri yok."}), 400
+    pattern = r'^[0-9.-]+$'
+    data=list(request.get_json().values())
+    for value in data:
+        if not re.match(pattern,value):
+            return jsonify({"error": "Geçersiz istek, hatali veri girisi."}), 400
+    np_data=np.array(data)
+    prediction=model.predict([np_data])
+    if prediction:
+        return "Zararlı tarama tespit edildi."
     else:
-        test=request.get_json()
-        if test is None:
-            return jsonify({"error": "Geçersiz istek, veri yok."}), 400
-        pattern = r'^[0-9.-]+$'
-        data=list(request.get_json().values())
-        for value in data:
-            if not re.match(pattern,value):
-                return jsonify({"error": "Geçersiz istek, hatali veri girisi."}), 400
-        np_data=np.array(data)
-        prediction=model.predict([np_data])
-        if prediction:
-            return "Zararlı tarama tespit edildi."
-        else:
-            return "Zararsız tarama tespit edildi."
+        return "Zararsız tarama tespit edildi."
 
 if __name__=="__main__":
     app.run(debug=False)
